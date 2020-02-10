282ca5924863131c91165420976ce8c45b2cbc0a
Maksim Rozhkov <kermit@mail.ru>
2020-02-10 23:21:23 +0300

    The Stack is now working! Thanks to "Go to Meals"

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoriesScreen = props => {
+
+    // Важно понимать, что все компоненты, которые участвуют
+    // в React Navigation получают через props дополнительное
+    // свойство, которое можно использовать при навигации.
+    // Имя этого свойства - "navigation". Через это свойство 
+    // можно вызвать различные вспомогательные функции, одной 
+    // из которых является navigate(). Эта функция разместит 
+    // над текущим экраном ещё один, из которого можно будет 
+    // вернуться на предыдущий экран по кнопке «Back». Именно
+    // так и реализуется Stack
 
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
+            <Button title="Go to Meals" onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals'})
+            }} />
         </View>
     );
 };
