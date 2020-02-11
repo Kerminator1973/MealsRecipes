869edad0a23d05d92a3e4e614286c920b46f13c7
Maksim Rozhkov <kermit@mail.ru>
2020-02-11 22:01:14 +0300

    One more nesting screen was added (Meal Details)

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoryMealScreen = props => {
 
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen!</Text>
+            <Button title="Go to Details" onPress={() => {
+                props.navigation.navigate({routeName: 'MealDetail'})
+            }} />            
         </View>
     );
 };
