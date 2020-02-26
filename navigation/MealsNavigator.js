c39706931ce29bcd3342758f12d65063f9650cd9
Maksim Rozhkov <kermit@mail.ru>
2020-02-26 22:34:53 +0300

    BottomTabNavigator was added as a main Nav

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Platform } from 'react-native';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen'; 
 import Colors from '../constants/Colors';
 
 // Определяем идентификаторы компонентов, которые могут быть
@@ -48,6 +50,22 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-// Оборачиваем страницы, которые используются в stack-модели
-// в специализированный контейнер
-export default createAppContainer(MealsNavigator);
+// Создаём ещё один навигационный орган управления - BottomTabNavigator.
+// Описание компонента очень похоже на описание элементов Stack-а.
+// Элементы Stack-а могут быть встроены в BottomTabNavigator - в нашем
+// случае, MealsNavigator встраивается в MealsFavTabNavigator
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {
+        screen: MealsNavigator
+    },
+    Favotites: FavoritesScreen
+});
+
+// Оборачиваем компонент, который является корневым компонентом
+// системы навигации специализированным контейнером.
+// Если бы мы хотели использовать в качестве навигационного
+// компонента только Stack, то следовало бы указать в качестве
+// корневого компонента MealsNavigator. Поскольку в приложении
+// корневой компонент - это BottomTabNavigator, указываем 
+// MealsFavTabNavigator
+export default createAppContainer(MealsFavTabNavigator);
