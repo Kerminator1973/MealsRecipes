848e7df6d5a6d0da848b4d313498cdec49c471fe
Maksim Rozhkov <kermit@mail.ru>
2020-02-28 23:10:57 +0300

    The Favorites Screen has changed

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import MealList from '../components/MealList';
+import { MEALS } from '../data/dummy-data';
+
 
 const FavoritesScreen = props => {
 
-    return (
-        <View style={styles.screen}>
-            <Text>The Favorites Screen!</Text>
-        </View>
-    );
+    // Извлекаем из dummy-data пару элементов, которые будут временно
+    // играть роль назначенных любимых блюд
+    const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+
+    return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
 // Настраиваем заголовок окна
 FavoritesScreen.navigationOptions = {
     headerTitle: 'Your Favorites'
 };
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
 
 export default FavoritesScreen;
