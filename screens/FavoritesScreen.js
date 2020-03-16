da7ce4b9302fd98c5e053442bfc637e04f4da233
Maksim Rozhkov <kermit@mail.ru>
2020-03-16 22:11:13 +0300

    The comment was added on FavoritesScreen is empty

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 import MealList from '../components/MealList';
 import { useSelector } from 'react-redux';
 
@@ -10,6 +12,14 @@ const FavoritesScreen = props => {
 
     // Используем Redux Store для доступа к глобальным свойствам приложения
     const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+    if (favMeals.length === 0 || !favMeals) {
+        return (
+        <View style={styles.content}>
+            <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+        </View>
+        );
+    }
 
     // Используем полученные данных для формирования списка блюд
     return <MealList listData={favMeals} navigation={props.navigation} />;
@@ -32,5 +42,13 @@ FavoritesScreen.navigationOptions = (navData) => {
         )        
     }
 };
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default FavoritesScreen;
